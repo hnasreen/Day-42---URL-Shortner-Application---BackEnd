9b466335e9f700d16ed274ae384f5f5ce8b6a50f
hnasreen <hnasreen1993@gmail.com>
2024-07-01 17:09:30 +0530

    modified the middleware code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization')?.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
@@ -12,6 +12,9 @@ const authMiddleware = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.userId);
+    if (!req.user) {
+      return res.status(401).json({ message: 'User not found, authorization denied' });
+    }
     next();
   } catch (error) {
     console.error(error);
